3833b2ddaf0dc34ce721cf11d546956194debc34
LWZ <411883104@qq.com>
2021-12-04 20:11:37 +0800

    Graph

--- DataStructureTest/test1/Maze.cpp
+++ DataStructureTest/test1/Maze.cpp
@@ -2,15 +2,25 @@
  * @Author: Outsider
  * @Date: 2021-11-13 13:29:10
  * @LastEditors: Outsider
- * @LastEditTime: 2021-11-29 14:39:10
+ * @LastEditTime: 2021-11-30 19:19:03
  * @Description: In User Settings Edit
  * @FilePath: \DataStructureTest\test1\Maze.cpp
  */
 
 #include <iostream>
 using namespace std;
-const int c = 300;
-char map[c][c]; //迷宫地图，1可行0不可行
+const int c = 9;
+//迷宫地图，1可行0不可行
+char map[c][c]={'1','1','1','1','1','1','1','1','1',
+                '1','1','1','1','1','0','0','0','1',
+                '1','0','1','0','1','0','0','1','1',
+                '1','0','1','0','1','1','1','1','0',
+                '1','1','1','1','1','0','0','0','0',
+                '1','1','0','0','1','0','1','1','1',
+                '1','1','1','0','0','1','1','0','1',
+                '1','0','1','1','1','1','0','1','0',
+                '1','0','0','0','1','0','0','0','1',
+                };
 int flag[c][c]; //标记是否遍历过
 
 int n, m; //地图的大下
@@ -22,10 +32,10 @@ struct Node
 { //定义节点表示坐标
     int x;
     int y;
+    Node* pre;  //记录该节点的上一个节点
     Node *next;  //作为队列时节点指针域
 };
 
-Node* res[c][c];
 
 struct Queue
 { //定义队列
@@ -52,14 +62,11 @@ Node* &front(Queue *queue)
 
 void pop(Queue *&queue)//头节点出队列
 {
-    Node *d=nullptr;
     if (queue->head != queue->tail)
     {
-        d = queue->head->next;
-        queue->head->next = queue->head->next->next;
-        if(d==queue->tail)
+        if(queue->head->next==queue->tail)
             queue->tail=queue->head;
-        delete d;
+        queue->head->next = queue->head->next->next;
     }
 }
 
@@ -80,79 +87,69 @@ Queue *queue = new Queue();
  * @param {int} end_y 终点y坐标
  * @return {*}
  */
+Node *now=new Node();
 void bfs(int x, int y,int end_x,int end_y)
 {
-    Node* first = new Node();//创建起始节点
-    first->x = x;
-    first->y = y;
-    flag[first->x][first->y]=1;
+    now->x=x;
+    now->y=y;
+    now->pre=nullptr;
 
-    Node* trck=new Node();//记录路径节点
-    trck->x=first->x;
-    trck->y=first->y;
-    res[first->x][first->y]=trck;
+    push(queue,now);
 
-    push(queue, first);
     while (!empty(queue))
     {
-        Node *now=front(queue);
-        if((now->x==end_x)&&(now->y==end_y))
+        now=front(queue);//取出队首元素分析
+        pop(queue);      //队首元素出队
+        if((now->x==end_x)&&(now->y==end_y))//到达终点
         {
             return;
         }
         for (int i = 0; i < 4; i++)
         {
-            Node *next = new Node();
-            next->x = now->x + dx[i];
-            next->y = now->y + dy[i];
-            if (!flag[next->x][next->y] && (map[next->x][next->y]=='1') && next->x > 0 && next->y > 0 && next->x <= n && next->y <= n)
+            int ax = now->x + dx[i];
+            int ay = now->y + dy[i];
+            if (!flag[ax][ay] && (map[ax][ay]=='1') && ax > 0 && ay > 0 && ax <= 8 && ay <= 8)
             {
+                Node *next = new Node();
                 push(queue, next);
-                flag[next->x][next->y] = 1;
-                trck=new Node();
-                trck->x=next->x;
-                trck->y=next->y;
-                res[next->x][next->y]=trck;
+                flag[ax][ay] = 1;
+                next->x=ax;
+                next->y=ay;
+                next->pre=now;
             }
-            else
-                delete next;
         }
-        pop(queue);
-        now=nullptr;
     }
 }
 
-void track()
-{
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(res[i][j])
-                cout<<res[i][j]->x<<" "<<res[i][j]->y<<endl;
+void print(){
+    if(now->x==8&&now->y==8)  //到达终点
+        while(now){
+            cout<<now->x<<" "<<now->y<<endl;
+            now=now->pre;
         }
-    }
+    else//没有到达终点
+        cout<<"Not Found!"<<endl;
 }
 
 int main()
 {
-
-    cin >> n >> m;
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= 8; i++)
     {
-        for (int j = 1; j <= m; j++)
-            cin >> map[i][j];
+        for (int j = 1; j <= 8; j++)
+           cout<< map[i][j]<<" ";
+        cout<<endl;
     }
-
     if (init(queue))
     {
-        bfs(1, 1,4,4);
+        bfs(1, 1,8,8);
     }
     
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= 8; i++)
     {
-        for (int j = 1; j <= m; j++)
+        for (int j = 1; j <= 8; j++)
             cout << flag[i][j] << ' ';
         cout << endl;
     }
-    track();
+    print();
     system("pause");
 }
